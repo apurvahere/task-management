39c07ffae182c1b5dfa85cdc01f602486b025ac4
apurvahere <apurvanayak836@gmail.com>
2026-03-10 12:34:04 +0530

    initial repo, dependencies and huskky pre-commit setup

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{ts,tsx}"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-4">
+        <h1 className="text-2xl font-bold">Task Management App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
